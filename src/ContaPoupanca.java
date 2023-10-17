10b73da390f340fa0bbccd499bce788afddb8961
Venilson Neves <venilsoncosta21@gmail.com>
2023-10-17 09:40:37 -0300

    criei uma conta poupanca que herda de conta bancaria

---
+++ src/ContaPoupanca.java
@@ -0,0 +1,5 @@
+public class ContaPoupanca extends ContaBancaria{
+    public ContaPoupanca(String nome, String agencia, String numeroConta, double saldo) {
+        super(nome, agencia, numeroConta, saldo);
+    }
+}
